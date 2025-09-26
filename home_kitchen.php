d0d0b0d9833b1fcf4d4801a780b520a3bb732687
Vijay chejara <vermavijay5125@gmail.com>
2025-09-26 15:32:17 +0530

    test

--- home_kitchen.php
+++ home_kitchen.php
@@ -15,6 +15,7 @@ include 'header.php';
             grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
             gap: 20px;
             padding: 20px;
+            
         }
         .product-card {
             background: #fff;
